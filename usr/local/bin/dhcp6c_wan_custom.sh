b6f111f3c23664a31f5d5927cb9f4c1f8174716b
Ted Tickell <ted@tickell.us>
2026-03-07 13:12:40 -0500

    Keep opnsense-ipv6 DHCPv6 tooling out of opnsense-ha installer

--- usr/local/bin/dhcp6c_wan_custom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Compatibility shim:
-# Existing dhcp6c wrapper scripts call /usr/local/bin/dhcp6c_wan_custom.sh.
-# Delegate all logic to the maintained HA hook implementation.
-
-exec /usr/local/bin/dhcp6c-wan-script.sh "$@"
